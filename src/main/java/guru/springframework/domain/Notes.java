2bf376215eb40b0db8b2f95cdc592f6c066606cb
Wendell Robles <wendellrobles@yahoo.com>
2021-08-22 21:09:33 -0500

    updated

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -3,24 +3,26 @@ package guru.springframework.domain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
 
-/**
- * Created by jt on 6/13/17.
- */
 @Data
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @OneToOne
-    private Recipe recipe;
+  @OneToOne
+  private Recipe recipe;
 
-    @Lob
-    private String recipeNotes;
+  @Lob
+  private String recipeNotes;
 
 }
--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -8,35 +8,36 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-/**
- * Created by jt on 6/17/17.
- */
+import static org.mockito.Mockito.anySet;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 public class IndexControllerTest {
 
-    @Mock
-    RecipeService recipeService;
+  @Mock
+  RecipeService recipeService;
 
-    @Mock
-    Model model;
+  @Mock
+  Model model;
 
-    IndexController controller;
+  IndexController controller;
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
 
-        controller = new IndexController(recipeService);
-    }
+    controller = new IndexController(recipeService);
+  }
 
-    @Test
-    public void getIndexPage() throws Exception {
+  @Test
+  public void getIndexPage() throws Exception {
 
-        String viewName = controller.getIndexPage(model);
+    String viewName = controller.getIndexPage(model);
 
-        assertEquals("index", viewName);
-        verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
-    }
+    assertEquals("index", viewName);
+    verify(recipeService, times(1)).getRecipes();
+    verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+  }
 
 }
