a0315e97e78cafac36f86fec5ef6fb9080b512a9
Wendell Robles <wendellrobles@yahoo.com>
2021-08-23 11:19:25 -0500

    updated

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -6,25 +6,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by jt on 6/1/17.
- */
 @Slf4j
 @Controller
 public class IndexController {
 
-    private final RecipeService recipeService;
+  private final RecipeService recipeService;
 
-    public IndexController(RecipeService recipeService) {
-        this.recipeService = recipeService;
-    }
+  public IndexController(RecipeService recipeService) {
+    this.recipeService = recipeService;
+  }
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(Model model) {
-        log.debug("Getting Index page");
+  @RequestMapping({"", "/", "/index"})
+  public String getIndexPage(Model model) {
+    log.debug("Getting Index page");
 
-        model.addAttribute("recipes", recipeService.getRecipes());
+    model.addAttribute("recipes", recipeService.getRecipes());
 
-        return "index";
-    }
+    return "index";
+  }
 }
--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -12,34 +12,31 @@ import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by jt on 6/17/17.
- */
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UnitOfMeasureRepositoryIT {
 
-    @Autowired
-    UnitOfMeasureRepository unitOfMeasureRepository;
+  @Autowired
+  UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @Before
-    public void setUp() throws Exception {
-    }
+  @Before
+  public void setUp() throws Exception {
+  }
 
-    @Test
-    public void findByDescription() throws Exception {
+  @Test
+  public void findByDescription() throws Exception {
 
-        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+    Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
 
-        assertEquals("Teaspoon", uomOptional.get().getDescription());
-    }
+    assertEquals("Teaspoon", uomOptional.get().getDescription());
+  }
 
-    @Test
-    public void findByDescriptionCup() throws Exception {
+  @Test
+  public void findByDescriptionCup() throws Exception {
 
-        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+    Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
 
-        assertEquals("Cup", uomOptional.get().getDescription());
-    }
+    assertEquals("Cup", uomOptional.get().getDescription());
+  }
 
 }
